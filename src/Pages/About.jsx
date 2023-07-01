01b2dce07a996c1938b3ffd881a283ebe2feccc0
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-07-01 13:06:46 +0600

    added about me text

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,3 +1,5 @@
+import { BsDashLg } from "react-icons/bs";
+
 const About = () => {
   return (
     <div id="about" className="py-28">
@@ -8,10 +10,10 @@ const About = () => {
         <h2 className="text-slate-100 text-6xl font-bold">About me</h2>
       </div>
       <div className="mt-10 mx-8 lg:mx-28">
-        <div className="grid lg:grid-cols-2 items-start gap-14">
+        <div className="grid lg:grid-cols-2 items-center justify-center gap-14">
           <div>
             <img
-              className="aboutImg w-full"
+              className="aboutImg mx-auto"
               src="https://i.ibb.co/ZBWt45G/rony.jpg"
               alt=""
             />
@@ -23,7 +25,24 @@ const About = () => {
               </button>
             </div>
           </div>
-          <div></div>
+          <div className="text-slate-300 text-lg">
+            <h1 className="text-3xl font-semibold">Hi! I am Muhammad Hamim.</h1>
+            <p className="text-justify">
+              <BsDashLg /> a passionate and versatile web developer with a
+              diverse skill set. My expertise spans across HTML, CSS, Tailwind,
+              Bootstrap, JavaScript, React, Node.js, Express.js, and MongoDB. I
+              thrive on turning creative ideas into practical, visually
+              appealing, and user-friendly websites. By combining design
+              aesthetics with technical prowess, I craft seamless and intuitive
+              user experiences. As a continuous learner, I stay updated with the
+              latest industry trends, ensuring my solutions adhere to best
+              practices. Whether it's building responsive front-end interfaces
+              or developing robust back-end functionalities, I am committed to
+              delivering high-quality results. Let's collaborate and embark on a
+              journey to bring your web projects to life with innovation and
+              excellence!
+            </p>
+          </div>
         </div>
       </div>
     </div>
